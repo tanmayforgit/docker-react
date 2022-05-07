a69e38c806f562a2837bb263615c1e54b43a1866
Tanmay Tupe <tanmaytupe@gmail.com>
2022-05-07 12:02:16 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          eagerly waiting for my amazon orders to come... yeah I am a slave to my addictions and material aspects!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
